b6068e74d55f08958b471570862fb39aba31db9c
ra <rallan_md@yahoo.ca>
2023-04-09 12:59:10 -0500

    created custom wait method with try-except and self.assertIn

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,33 +2,39 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
-        #self.browser = webdriver.Chrome()
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID,'id_list_table')
+                rows = table.find_elements(By.TAG_NAME,'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        # self.browser.get('http://localhost:8001')
         self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
-        # title_text = self.browser.find_element(By.TAG_NAME,'title').text
-        # time.sleep(1)
-        # self.assertIn('To-Do', title_text )
         self.assertIn("To-Do", self.browser.title)
   
         header_text = self.browser.find_element(By.TAG_NAME,'h1').text
@@ -40,28 +46,27 @@ class NewVisitorTest(LiveServerTestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys("Buy peacock feathers")
+
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very
-# methodical)
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows], f"New to-do item did not appear in table. Contents were:\n{table.text}")
-        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows], f"New to-do item did not appear in table. Contents were:\n{table.text}")
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
         self.fail('Finish the test!')
-# She visits that URL - her to-do list is still there.
+        # She visits that URL - her to-do list is still there.
 
