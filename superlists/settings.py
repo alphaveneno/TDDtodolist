80d60d7965eeae37e2ed0f614cf97d3c987fc15d
ra <rallan_md@yahoo.ca>
2023-04-09 21:31:37 -0500

    end of ch08, Harry Percival's code needed to be brought up-to-date e'g, find_element_by_id -> find_element(By.ID,

--- superlists/settings.py
+++ superlists/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,9 +25,8 @@ SECRET_KEY = 'django-insecure-#$i(cmss=)b=5m^i(n4^q2aheonc$b9juo&3r=9_l3h)c*b9oy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['localhost',
-#                  '127.0.0.1', ]
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1', ]
 
 
 # Application definition
@@ -80,7 +79,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -120,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
